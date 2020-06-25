f165ec0175199acb32eaab81cdc9ea62a2e1922d
Melanie Gutzmann <mirrorkeydev@gmail.com>
2020-06-25 08:54:23 -0700

    Add home link to header name

--- portfolio/src/main/webapp/components/navheader.js
+++ portfolio/src/main/webapp/components/navheader.js
@@ -1,11 +1,11 @@
 const NavHeaderTemplate = 
 `<div id="header-container">
     <img id="header-img" src="./assets/me.jpg" alt="Profile picture">
-    <span id="header">Melanie Gutzmann</span>
+    <router-link id="header" to="/">Melanie Gutzmann</router-link>
     <div id="nav-links">
         <router-link class="nav-link" to="/about">About</router-link>
-        <router-link class="nav-link" href="/projects">Projects</router-link>
-        <router-link class="nav-link" href="/contact">Contact</router-link>
+        <router-link class="nav-link" to="/projects">Projects</router-link>
+        <router-link class="nav-link" to="/contact">Contact</router-link>
     </div>
 </div>`;
 
