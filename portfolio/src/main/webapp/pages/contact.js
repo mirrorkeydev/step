ae3abeeb8aab0971005faa4db6c2c8a43e29e412
Melanie Gutzmann <mirrorkeydev@gmail.com>
2020-06-25 16:16:31 -0700

    Add basic contact page

---
+++ portfolio/src/main/webapp/pages/contact.js
@@ -0,0 +1,46 @@
+/*
+ * The contact page of the website.
+ */
+
+const ContactTemplate = 
+`<div id="body-container">
+    <TextBox>
+
+    <h2 class="centered">Get in Touch!</h2>
+    <br/>
+
+    <div class="icontitle-group">
+        
+        <a class="icon-link" href="mailto:mirrorkeydev@gmail.com">
+            <IconTitle icon="fas fa-envelope fa-2x">mirrorkeydev@gmail.com</IconTitle>
+        </a>
+
+        <a class="icon-link" href="https://www.linkedin.com/in/mgutzmann">
+            <IconTitle icon="fab fa-linkedin-in fa-2x">linkedin.com/in/mgutzmann</IconTitle>
+        </a>
+
+        <a class="icon-link" href="https://github.com/mirrorkeydev">
+            <IconTitle icon="fab fa-github fa-2x">github.com/mirrorkeydev</IconTitle>
+        </a>
+
+        <a class="icon-link" href="https://open.spotify.com/user/mirrorkey?si=HbKiiZDRRdCicNRs_qoKtg">
+            <IconTitle icon="fab fa-spotify fa-2x">mirrorkey</IconTitle>
+        </a>
+
+    </div>
+
+  </TextBox>
+</div>`;
+
+import { TextBox } from '../components/textbox.js';
+import { IconTitle } from '../components/icontitle.js';
+
+const Contact = {
+    template: ContactTemplate,
+    components: {
+        'TextBox': TextBox,
+        'IconTitle': IconTitle,
+    },
+};
+
+export { Contact };
