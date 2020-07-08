58deb3bdb01de5be6248bd72579d34dbeb8f4fbf
Melanie Gutzmann <mirrorkeydev@gmail.com>
2020-07-08 00:06:13 +0000

    Fix Java and css style

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -40,116 +40,112 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-    public class Comment {
-        String body;
-        String author;
-        Date date;
-        Double sentiment;
+  public class Comment {
+    String body;
+    String author;
+    Date date;
+    Double sentiment;
 
-        public Comment(String body, String author, Date date, Double sentiment) {
-            this.body = body;
-            this.author = author;
-            this.date = date;
-            this.sentiment = sentiment;
-        }
+    public Comment(String body, String author, Date date, Double sentiment) {
+      this.body = body;
+      this.author = author;
+      this.date = date;
+      this.sentiment = sentiment;
+    }
+  }
+
+  /** GETs a user-defined number of comments translated to a user-defined language. */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Get the input from the form.
+    int numberOfCommentsToShow = 0;
+    if (request.getParameter("num-comments") != null && !request.getParameter("num-comments").isEmpty()){
+      try {
+        numberOfCommentsToShow = Integer.parseInt(request.getParameter("num-comments"));
+      } catch (Exception e) {
+        // If parsing fails (non-numeric input), we silently fall back on showing 10 comments
+        numberOfCommentsToShow = 10;
+      }
     }
 
-    /** GETs a user-defined number of comments translated to a user-defined language. */
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("Comment").addSort("datetime", SortDirection.DESCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    List<Entity> results = numberOfCommentsToShow >= 0 ? datastore.prepare(query).asList(FetchOptions.Builder.withLimit(numberOfCommentsToShow))
+                                                      : datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
 
-        // Get the input from the form.
-        int numberOfCommentsToShow = 0;
-        if (request.getParameter("num-comments") != null && !request.getParameter("num-comments").isEmpty()){
-            try {
-                numberOfCommentsToShow = Integer.parseInt(request.getParameter("num-comments"));
-            } catch (Exception e) {
-                // If parsing fails (non-numeric input), we silently fall back on showing 10 comments
-                numberOfCommentsToShow = 10;
-            }
-        }
+    ArrayList<Comment> comments = new ArrayList<Comment>();
+    for (Entity entity : results) {
+      String author = (String) entity.getProperty("author");
+      String body = (String) entity.getProperty("body");
+      Date datetime = (Date) entity.getProperty("datetime");
+      Double sentiment = (Double) entity.getProperty("sentiment");
 
-        Query query = new Query("Comment").addSort("datetime", SortDirection.DESCENDING);
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        List<Entity> results = numberOfCommentsToShow >= 0 ? datastore.prepare(query).asList(FetchOptions.Builder.withLimit(numberOfCommentsToShow))
-                                                          : datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
-
-        ArrayList<Comment> comments = new ArrayList<Comment>();
-        for (Entity entity : results) {
-            String author = (String) entity.getProperty("author");
-            String body = (String) entity.getProperty("body");
-            Date datetime = (Date) entity.getProperty("datetime");
-            Double sentiment = (Double) entity.getProperty("sentiment");
-
-            Comment comment = new Comment(body, author, datetime, sentiment);
-            comments.add(comment);
-        }
-
-        // Do the translation.
-        String languageToTranslateTo = request.getParameter("lang-comments");
-        if (!languageToTranslateTo.equals("original")) {
-            Translate translate = TranslateOptions.getDefaultInstance().getService();
-            for (Comment comment : comments) {
-                Translation translation =
-                    translate.translate(comment.body, 
-                    Translate.TranslateOption.targetLanguage(languageToTranslateTo),
-                    Translate.TranslateOption.format("text"));
-                String translatedText = translation.getTranslatedText();
-                comment.body = translatedText;
-            }
-        }
-
-        Gson gson = new Gson();
-        String json = gson.toJson(comments);
-
-        response.setContentType("text/json; charset=UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().println(json);
+      Comment comment = new Comment(body, author, datetime, sentiment);
+      comments.add(comment);
     }
 
-    /** POST a new comment to the server. */
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-        // Get the input from the form.
-        String body = getParameter(request, "body", "");
-        String author = getParameter(request, "author", "Anonymous");
-        Date datetime = new Date();
-
-        // Check for validity.
-        if (body.isEmpty()) {
-            response.setStatus(400);
-            return;
-        }   
-        
-        // Check the sentiment of the comment entered
-        Document doc =
-            Document.newBuilder().setContent(body).setType(Document.Type.PLAIN_TEXT).build();
-        LanguageServiceClient languageService = LanguageServiceClient.create();
-        Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
-        float score = sentiment.getScore();
-        languageService.close();
-
-        response.setStatus(200);
-        response.getWriter().println(score);
-
-        if (score < -0.7) {
-            return;
-        }
-
-        // Buld the new comment.
-        Entity commentEntity = new Entity("Comment");
-        commentEntity.setProperty("author", author);
-        commentEntity.setProperty("body", body);
-        commentEntity.setProperty("datetime", datetime);
-        commentEntity.setProperty("sentiment", score);
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        datastore.put(commentEntity);
+    // Do the translation.
+    String languageToTranslateTo = request.getParameter("lang-comments");
+    if (!languageToTranslateTo.equals("original")) {
+      Translate translate = TranslateOptions.getDefaultInstance().getService();
+      for (Comment comment : comments) {
+        Translation translation =
+          translate.translate(comment.body, 
+          Translate.TranslateOption.targetLanguage(languageToTranslateTo),
+          Translate.TranslateOption.format("text"));
+        String translatedText = translation.getTranslatedText();
+        comment.body = translatedText;
+      }
     }
 
-    // Redirect back to the Contact page.
-    response.sendRedirect("/#/contact");
+    Gson gson = new Gson();
+    String json = gson.toJson(comments);
+
+    response.setContentType("text/json; charset=UTF-8");
+    response.setCharacterEncoding("UTF-8");
+    response.getWriter().println(json);
+  }
+
+  /** POST a new comment to the server. */
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Get the input from the form.
+    String body = getParameter(request, "body", "");
+    String author = getParameter(request, "author", "Anonymous");
+    Date datetime = new Date();
+
+    // Check for validity.
+    if (body.isEmpty()) {
+      response.setStatus(400);
+      return;
+    }   
+      
+    // Check the sentiment of the comment entered
+    Document doc =
+        Document.newBuilder().setContent(body).setType(Document.Type.PLAIN_TEXT).build();
+    LanguageServiceClient languageService = LanguageServiceClient.create();
+    Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
+    float score = sentiment.getScore();
+    languageService.close();
+
+    response.setStatus(200);
+    response.getWriter().println(score);
+
+    if (score < -0.7) {
+      return;
+    }
+
+    // Buld the new comment.
+    Entity commentEntity = new Entity("Comment");
+    commentEntity.setProperty("author", author);
+    commentEntity.setProperty("body", body);
+    commentEntity.setProperty("datetime", datetime);
+    commentEntity.setProperty("sentiment", score);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(commentEntity);
   }
 
   /**
